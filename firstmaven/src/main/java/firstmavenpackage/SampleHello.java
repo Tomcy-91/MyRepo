fbd4c1c2f47bff529321fba4b877649d87818e69
tomcy <tomcy@192.168.1.38>
2024-08-07 20:59:46 +0530

    Sample push1

---
+++ firstmaven/src/main/java/firstmavenpackage/SampleHello.java
@@ -0,0 +1,12 @@
+package firstmavenpackage;
+
+public class SampleHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "Tom";
+		System.out.println(a);
+		
+	}
+
+}
